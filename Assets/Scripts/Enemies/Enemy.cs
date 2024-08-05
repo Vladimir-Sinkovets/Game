b531aafd1d3ac9315cb2c463177a2302c4e1e885
Vladimir Sinkovets <vovasin49@gmail.com>
2024-08-05 16:25:34 +0300

    Add enemy script

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Enemies
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody;
+
+        public void MoveTo(Vector3 target)
+        {
+
+        }
+    }
+}
