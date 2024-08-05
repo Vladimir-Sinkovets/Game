c2eb0ebaa55defb28b2b3d5ef9b2b0a68f525bda
Vladimir Sinkovets <vovasin49@gmail.com>
2024-08-05 16:48:13 +0300

    add enemies controller

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,11 +4,14 @@ namespace Assets.Scripts.Enemies
 {
     public class Enemy : MonoBehaviour
     {
-        [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private float _speed;
 
         public void MoveTo(Vector3 target)
         {
+            var heading = target - transform.position;
+            var direction = heading / heading.magnitude;
 
+            transform.Translate(heading * _speed);
         }
     }
 }
---
+++ Assets/Scripts/Services/EnemiesController/EnemiesController.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Enemies;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Services.EnemiesController
+{
+    public class EnemiesController : MonoBehaviour
+    {
+        [SerializeField] private List<Enemy> _enemies;
+        [SerializeField] private Transform _target;
+
+        private void Update()
+        {
+            foreach (Enemy enemy in _enemies)
+            {
+                enemy.MoveTo(_target.position);
+            }
+        }
+    }
+}
