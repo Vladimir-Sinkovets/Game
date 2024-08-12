613931a2999e533de33d16a1c0a5695a08167a7d
Vladimir Sinkovets <vovasin49@gmail.com>
2024-08-12 09:00:00 +0300

    add menu scene

---
+++ Assets/Scripts/Installers/MenuInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace Assets.Scripts.Installers
+{
+    public class MenuInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+
+        }
+    }
+}
